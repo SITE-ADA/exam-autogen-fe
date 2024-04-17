22f95df43c09657e6e4aebc972b3267cf0277f4c
Aslan <aslan.ibadullayev.2003@gmail.com>
2024-04-18 03:56:30 +0400

    implemented breadcrumbs

--- src/components/Breadcrumb/Breadcrumbs.jsx
+++ src/components/Breadcrumb/Breadcrumbs.jsx
@@ -1,87 +1,64 @@
-import React from 'react';
-import { Link, NavLink, useLocation, useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import styles from './Breadcrumbs.module.css';
+import { msQuestionApi } from '../../Services/AxiosService';
 
-const Breadcrumbs = ({ questionPoolName }) => {
-  const {QuestionPoolName, QuestionId} = useParams();
+const Breadcrumbs = () => {
   const location = useLocation();
-  
-  const pathnames = location.pathname.slice(1, location.pathname.length).split('/');
-  console.log(pathnames);
-  let arr = [];
-  let newArr = [];
-  const crumbs = pathnames.filter((crumb) => crumb !== '');
-  
-  const BuildLink = (newArr) =>
-  {
-    let link = "Instructor";
-    for(let i = 0; i < newArr.length; i++)
-    {
-      if(newArr[i] === 'Question Pools')
-      link =+ '/QuestionPools';
-      if(newArr[i] === 'Question Pool' && newArr[i + 1] === 'Create Question')
-      link =+ '/2';
-      if(newArr[i] === 'Question Pools' && newArr[i + 1] === 'Create Question Pool')
-      link =+ '/CreateQuestionPool/0';
-      if(newArr[i] === 'Question Pools' && newArr[i + 1] === 'Edit Question Pool')
-      link =+ '/EditQuestionPool/23';
-      if(newArr[i] === 'Question Pools' && newArr[i + 2] === 'Create Question')
-      link =+ '/CreateQuestion/0';
-      if(newArr[i] === 'Question Pools' && newArr[i + 2] === 'Edit Question')
-      link =+ '/EditQuestion/21';
+  const { questionPoolId } = useParams();
+  const entirePath = location.pathname.split('/').filter((path) => path !== '');
+  const paths = entirePath.slice(0, entirePath.length + 1);
+  const [questionPoolName, setQuestionPoolName] = useState('');
+  const isInstructorPage = paths[0] === 'Instructor'; 
+
+
+  const fetchQuestionPoolName = async () => {
+    try {
+      const response = await msQuestionApi(`/question-pool/${questionPoolId}`);
+      const name = response.data.name;
+      console.log(name)      
+      setQuestionPoolName(name);
+    } catch (error) {
+      console.log("ef");
+      console.error('Error fetching question pool name:', error);
     }
-    console.log(link);
-    return link;
-  }
-    
-  
+  };
+
   useEffect(() => {
-    //console.log(BuildLink(2));
-  }, [location])
-  
-  for(let i = 0; i < crumbs.length; i++)
-  {
-    if(crumbs[i] === 'QuestionPools')
-      arr.push('Question Pools')
-    else if (crumbs[i] === 'CreateQuestionPool')
-      arr.push('Create Question Pool')
-    else if (crumbs[i] === 'EditQuestionPool')
-      arr.push('Edit Question Pool')
-    else if (crumbs[i - 1] === 'QuestionPools' && crumbs[i + 1] !== 'CreateQuestion' && crumbs[i + 1] !== 'EditQuestion')
-      arr.push('Question Pool');
-     else if (crumbs[i - 1] === 'QuestionPools' && (crumbs[i + 1] === 'CreateQuestion' || crumbs[i + 1] === 'EditQuestion'))
-      arr.push('Question Pool');
-    else if (crumbs[i] === 'CreateQuestion')
-      arr.push('Create Question')
-    else if (crumbs[i] === 'EditQuestion' && crumbs[i + 1] !== 0)
-      arr.push('Edit Question')
-    
-  }
-  
-  console.log(arr)
-  // console.log(arr)
+
+    fetchQuestionPoolName();
+
+  }, [questionPoolId]);
 
   return (
-    <nav aria-label="Breadcrumb">
-      <ul className={styles.breadcrumb}>
-        {
-          arr?.slice(0, arr.length - 1).map((crumb, index) =>
-          {
-            newArr.push(crumb);
-            return (
-            
-            <li key={index}>
-                <NavLink to={`/${BuildLink(newArr)}`}>{crumb}</NavLink>
-            </li>
-            )
-          })
-        }
-        <li><span>{arr[arr.length - 1]}</span></li>
-      </ul>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        {/* Render Home breadcrumb only if it's not /Instructor/QuestionPools */}
+        {paths.map((path, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && <span className={styles.breadcrumb_separator}> &gt; </span>}
+            {/* Display 'QuestionPools' as default if endpoint is /Instructor/ or /Instructor */}
+            {(paths.length === 1 && paths[0] === 'Instructor' && index === 0) ? (
+              <li className={styles.breadcrumb_item}>
+                <Link to="/Instructor/QuestionPools">Question Pools</Link>
+              </li> 
+            ) : 
+            (
+              <li className={styles.breadcrumb_item}>
+                <Link to={`/${paths.slice(0, index + 1).join('/')}`}>
+                {index === paths.length - 1 && !isNaN(path) && paths[index - 1] === 'QuestionPools' ? (
+        questionPoolName || path // Display question pool name if available, otherwise display parameter
+      ) : (
+        path
+      )}
+                </Link>
+              </li>
+            )}
+          </React.Fragment>
+        ))}
+      </ol>
     </nav>
   );
-}
-
+};
 
 export default Breadcrumbs;
