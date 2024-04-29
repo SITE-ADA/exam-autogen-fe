5048717a22c64e0526f3c5146bc897b6687813f6
Aslan <aslan.ibadullayev.2003@gmail.com>
2024-04-29 05:04:33 +0400

    added file input to assessment

--- src/components/Instructor/Assessment/Assessment.jsx
+++ src/components/Instructor/Assessment/Assessment.jsx
@@ -3,6 +3,11 @@ import styles from './Assessment.module.css';
 import Cloud from '../../../icons/buttons-icons/cloud.svg';
 export const Assessment = () =>
 {
+
+    const handleUploadClick = () => {
+        document.getElementById('fileInput').click();
+      };
+    
     return (
         <div className={styles.assessment_page}>
             <h3>Assessment</h3>
@@ -11,7 +16,7 @@ export const Assessment = () =>
                 <p>Upload the answer sheet</p>
                     <div className={styles.icon_horizontal}>
                         <div className={styles.left_line}></div>
-                            <span className={styles.test_create_icon}><img src={Cloud} alt="" /></span>
+                            <span onClick={() => handleUploadClick()} className={styles.test_create_icon}><img src={Cloud} alt="" /><input type="file" id="fileInput" className={styles.file_input}/></span>
                         <div className={styles.right_line}></div>
                     </div>
                 <p className={styles.icon_info}>Click the icon above to get started</p>
